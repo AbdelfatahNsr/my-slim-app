3ce52d8f501144940eb82cea2b35bbda2ed7a6b2
Abdel <abdcoo1@gmail.com>
2023-09-27 21:38:39 +0200

    added etage middleware

--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
-
+use App\Application\Middleware\ETagMiddleware;
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
         // CORS Pre-Flight OPTIONS Request Handler
@@ -57,5 +57,5 @@ return function (App $app) {
         $response = $response->withHeader('Content-Type', 'application/json');
     
         return $response;
-    });
+    })->add(new ETagMiddleware());
 };
